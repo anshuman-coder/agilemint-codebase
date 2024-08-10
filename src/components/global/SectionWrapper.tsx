904ec5cca8006c104c6b930f3f051de880b5bf64
anshuman-coder <anshurockingboy23@gmail.com>
2024-08-11 01:12:24 +0530

    section wrapper and other components

---
+++ src/components/global/SectionWrapper.tsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx'
+import React, { FC, PropsWithChildren } from 'react'
+import { Container } from '~/components/global'
+
+interface SectionWrapper extends PropsWithChildren {
+  title?: string
+  preTitle?: string
+  align?: 'left' | 'center' | 'right'
+}
+
+const SectionWrapper: FC<SectionWrapper> = ({
+  title = '',
+  preTitle = '',
+  align = 'left',
+  children,
+}) => {
+  return (
+    <Container
+      className={
+        clsx(
+          'flex w-full flex-col mt-4',
+          'items-center justify-center',
+          align === 'center' && 'text-center',
+          align === 'left' && 'text-left',
+          align === 'right' && 'text-right',
+        )
+      }
+    >
+      {
+        preTitle && (
+          <div className='text-sm font-bold tracking-wider text-indigo-600 uppercase'>
+            {preTitle}
+          </div>
+        )
+      }
+
+      {
+        title && (
+          <h2 className='max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white'>
+            {title}
+          </h2>
+        )
+      }
+      {
+        children && (
+          <p className='max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300'>
+            {children}
+          </p>
+        )
+      }
+    </Container>
+  )
+}
+
+export default SectionWrapper
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PageBox, PageHelmet } from '~/components/global'
+import { PageBox, PageHelmet, SectionWrapper } from '~/components/global'
 import Hero from '~/components/home/Hero'
 
 export default function Home() {
@@ -8,6 +8,15 @@ export default function Home() {
       <PageHelmet />
       <PageBox>
         <Hero />
+        <SectionWrapper
+          preTitle='Nextly Benefits'
+          title='Why should you use this landing page'
+          align='center'
+        >
+          Nextly is a free landing page & marketing website template for startups
+          and indie projects. Its built with Next.js & TailwindCSS. And its
+          completely open-source.
+        </SectionWrapper>
       </PageBox>
     </>
   )
