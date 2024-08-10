e2d8663c1349fd6090ee381e43b6ab7f062767d3
anshuman-coder <anshurockingboy23@gmail.com>
2024-08-10 17:27:02 +0530

    global components so far

---
+++ src/components/global/PageHelmet.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import React, { FC } from 'react'
+
+interface PageHelmetProps {
+  title?: string
+}
+
+const PageHelmet: FC<PageHelmetProps> = ({
+  title = ''
+}) => {
+  return (
+    <Head>
+      <title>AgileMint {title ? `${title}` : ''}</title>
+      <meta
+        name='description'
+        content='Nextly is a free landing page template built with next.js & Tailwind CSS'
+      />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  )
+}
+
+export default PageHelmet
---
+++ src/components/global/index.tsx
@@ -0,0 +1,3 @@
+export { default as PageBox } from './PageBox'
+export { default as ThemeSwitch } from './ThemeSwitch'
+export { default as PageHelmet } from './PageHelmet'
